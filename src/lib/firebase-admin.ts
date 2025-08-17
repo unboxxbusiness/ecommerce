d0127b985f895cafca918a34363a92baf9f3a1c4
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-17 16:52:35 +0000

    Runtime Error
    Error: Could not initialize Firebase Admin SDK
    
    src/lib/

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -5,28 +5,19 @@ import * as admin from 'firebase-admin';
 if (!admin.apps.length) {
   const privateKey = process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
-  if (!process.env.FB_PROJECT_ID) {
-    throw new Error('Missing Firebase project ID');
-  }
-  if (!process.env.FB_CLIENT_EMAIL) {
-    throw new Error('Missing Firebase client email');
-  }
-  if (!privateKey) {
-    throw new Error('Missing Firebase private key');
+  if (!process.env.FB_PROJECT_ID || !process.env.FB_CLIENT_EMAIL || !privateKey) {
+    throw new Error(
+      'Missing Firebase Admin SDK credentials. Please check your .env.local file.'
+    );
   }
 
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FB_PROJECT_ID,
-        clientEmail: process.env.FB_CLIENT_EMAIL,
-        privateKey,
-      }),
-    });
-  } catch (error: any) {
-    console.error('Firebase admin initialization error', error.stack);
-    throw new Error('Could not initialize Firebase Admin SDK');
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FB_PROJECT_ID,
+      clientEmail: process.env.FB_CLIENT_EMAIL,
+      privateKey,
+    }),
+  });
 }
 
 const adminDb = admin.firestore();
