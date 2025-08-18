5eb56d77ace816881580f31fb51c23dbabc4ac44
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-18 10:28:55 +0000

    make outside this hide and show icon from sidebar

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -8,7 +8,6 @@ import {
   SidebarHeader,
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from '@/components/ui/sidebar';
 import { Nav } from '@/nav';
 import { Button } from '@/components/ui/button';
@@ -44,14 +43,13 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <Sidebar>
-        <SidebarHeader className="flex items-center justify-between">
+        <SidebarHeader className="flex items-center">
           <div className="flex items-center gap-2">
              <Button variant="ghost" size="icon" className="shrink-0 text-primary hover:bg-primary/10 hover:text-primary">
                 <Gem className="size-5" />
              </Button>
             <span className="font-headline text-lg font-semibold group-data-[collapsible=icon]:hidden">Digital Shop</span>
           </div>
-           <SidebarTrigger />
         </SidebarHeader>
         <SidebarContent>
           <Nav />
