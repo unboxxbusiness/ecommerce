279a31a3c1f54b61d88e2af35f7655af48b39d28
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-19 12:10:26 +0000

    Console Error
    FirebaseError: Messaging: We are unable to register the

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,27 +1,42 @@
 
-// This file needs to be in the public directory
+// This file needs to be in the public folder.
 
 // Scripts for firebase and firebase messaging
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker with the App's configuration
-const params = new URL(location).searchParams;
-const firebaseConfig = Object.fromEntries(params.entries());
+// Get the Firebase config from the URL query parameters
+const urlParams = new URLSearchParams(location.search);
+const firebaseConfig = {
+    apiKey: urlParams.get('apiKey'),
+    authDomain: urlParams.get('authDomain'),
+    projectId: urlParams.get('projectId'),
+    storageBucket: urlParams.get('storageBucket'),
+    messagingSenderId: urlParams.get('messagingSenderId'),
+    appId: urlParams.get('appId'),
+};
 
-firebase.initializeApp(firebaseConfig);
+// Initialize the Firebase app in the service worker with the retrieved config
+if (firebaseConfig.apiKey) {
+    firebase.initializeApp(firebaseConfig);
 
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
-const messaging = firebase.messaging();
+    // Retrieve an instance of Firebase Messaging so that it can handle background messages.
+    const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    messaging.onBackgroundMessage((payload) => {
+        console.log(
+            '[firebase-messaging-sw.js] Received background message ',
+            payload
+        );
+        // Customize notification here
+        const notificationTitle = payload.notification.title;
+        const notificationOptions = {
+            body: payload.notification.body,
+            icon: '/icon-192x192.png'
+        };
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon-192x192.png' // Or your app's icon
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+        self.registration.showNotification(notificationTitle, notificationOptions);
+    });
+} else {
+    console.error("Firebase config not found in service worker. Notifications will not work.");
+}
