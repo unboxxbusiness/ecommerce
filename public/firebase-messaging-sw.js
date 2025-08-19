e57d3211c7598f27ed7c4c03603870ae256e8508
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-19 07:22:55 +0000

    Console Error
    FirebaseError: Messaging: We are unable to register the

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,28 +1,32 @@
 
-// Scripts for Firebase products are imported in the HTML file, so they are available to the service worker.
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+// Import the Firebase app and messaging scripts
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
-// Note: This is safe to expose. GCM/FCM use this to identify the app.
+// Get the Firebase config from the query string
 const urlParams = new URLSearchParams(location.search);
-const firebaseConfig = Object.fromEntries(urlParams.entries());
+const firebaseConfig = {
+    apiKey: urlParams.get('apiKey'),
+    authDomain: urlParams.get('authDomain'),
+    projectId: urlParams.get('projectId'),
+    storageBucket: urlParams.get('storageBucket'),
+    messagingSenderId: urlParams.get('messagingSenderId'),
+    appId: urlParams.get('appId'),
+};
 
+// Initialize the Firebase app in the service worker
 firebase.initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  );
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/logo.png' // Optional: path to an icon
+    icon: '/icon-192x192.png' // Or your app's icon
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
